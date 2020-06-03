5e773834eab1c8e4ce712fad99683d672eea7759
Bykov Dima <fepifmel@gmail.com>
2020-06-03 16:23:49 +0300

    анимации

--- frontend/src/components/Buisness/Services.vue
+++ frontend/src/components/Buisness/Services.vue
@@ -1,21 +1,26 @@
 <template>
   <section class="section services">
-
     <div class="container">
       <ButtonBack class="services__btn-back" :to="'/about'" />
       <div class="services__cols">
-        <div class="services__col-narrow ">
-            <div class="services__item services__item--stock">
-              <div class="services__item-wrapper services__item-wrapper--text services__item-wrapper--scrollbar scrollbar">
-                <p class="services__item-text services__item-text--stock">Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании</p>
-              </div>
-
-              <div class="services__item-wrapper services__item-wrapper--red">
-                <p class="services__item-price services__item-price--stock">от
-                  <span class="services__item-number services__item-number--stock">100 000</span>
-                   руб.</p>
-              </div>
+        <div class="services__col-narrow">
+          <div class="services__item services__item--stock">
+            <div
+              class="services__item-wrapper services__item-wrapper--text services__item-wrapper--scrollbar scrollbar"
+            >
+              <p
+                class="services__item-text services__item-text--stock"
+              >Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компанииПроведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании</p>
             </div>
+
+            <div class="services__item-wrapper services__item-wrapper--red">
+              <p class="services__item-price services__item-price--stock">
+                от
+                <span class="services__item-number services__item-number--stock">100 000</span>
+                руб.
+              </p>
+            </div>
+          </div>
         </div>
 
         <div class="services__col-wide">
@@ -25,15 +30,19 @@
                 <p class="services__item-text services__item-text--right">{{item.text}}</p>
               </div>
               <div class="services__item-wrapper services__item-wrapper--number">
-                <p v-if="item.oldPrice" class="services__item-price services__item-price--old">от <span class="services__item-number services__item-number--old">{{item.oldPrice}}</span> руб.</p>
-                <p class="services__item-price">от
+                <p v-if="item.oldPrice" class="services__item-price services__item-price--old">
+                  от
+                  <span class="services__item-number services__item-number--old">{{item.oldPrice}}</span> руб.
+                </p>
+                <p class="services__item-price">
+                  от
                   <span class="services__item-number">{{item.price}}</span>
-                  руб.</p>
+                  руб.
+                </p>
               </div>
             </li>
           </ul>
         </div>
-
       </div>
 
       <Breadcrumbs :links="links" />
@@ -70,27 +79,29 @@ export default {
       items: [
         {
           id: 1,
-          text: 'Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании',
-          price: '1223',
-          oldPrice: '12512'
+          text:
+            "Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании",
+          price: "1223",
+          oldPrice: "12512"
         },
         {
           id: 2,
-          text: 'Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании',
-          price: '1223',
+          text:
+            "Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании",
+          price: "1223"
         },
         {
           id: 3,
-          text: 'Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании',
-          price: '1223',
-        },
+          text:
+            "Проведение квеста «Чистые игры» для укрепления корпоративной культуры и формирования имиджа компании",
+          price: "1223"
+        }
       ]
-    }
+    };
   },
 
   mounted() {
-    if (this.items.length > 2)
-       this.scroll = true
+    if (this.items.length > 2) this.scroll = true;
   }
 };
 </script>
--- frontend/src/components/controls/VSlider.vue
+++ frontend/src/components/controls/VSlider.vue
@@ -1,86 +1,84 @@
 <template>
-    <div class="v-slider" v-swiper:slider="options">
-        <button class="swiper-button-prev">
-                <v-svg name="arrow_next" width="40" height="40" />
-            </button>
-        <div class="swiper-wrapper">
-            <slot />
-        </div>
-        <button class="swiper-button-next">
-            <v-svg name="arrow_next" width="40" height="40" />
-        </button>
-        <div class="swiper-pagination" />
+  <div class="v-slider" v-swiper:slider="options">
+    <button class="swiper-button-prev">
+      <v-svg name="arrow_next" width="40" height="40" />
+    </button>
+    <div class="swiper-wrapper">
+      <slot />
     </div>
+    <button class="swiper-button-next">
+      <v-svg name="arrow_next" width="40" height="40" />
+    </button>
+    <div class="swiper-pagination" />
+  </div>
 </template>
 
 <script>
-import VSvg from './VSvg.vue';
+import VSvg from "./VSvg.vue";
 
-import {  directive } from 'vue-awesome-swiper'
-
+import { directive } from "vue-awesome-swiper";
 
 export default {
-    name: 'v-slider',
-    components: {
-        VSvg,
+  name: "v-slider",
+  components: {
+    VSvg
+  },
+  directives: {
+    swiper: directive
+  },
 
-    },
-    directives: {
-        swiper: directive
-    },
+  serverCacheKey: props => props.name,
 
-    serverCacheKey: props => props.name,
-
-    props: {
-        name: {
-            type: String,
-            required: true,
-        },
-
-        options: {
-            type: Object,
-            default() {
-                return {
-                    init: false,
-                    grabCursor: true,
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
-                };
-            },
-        },
-
-        shouldInitialize: {
-            type: Boolean,
-            default: true,
-        },
+  props: {
+    name: {
+      type: String,
+      required: true
     },
 
-    watch: {
-        shouldInitialize(value) {
-            if (value && !this.initialized) this.slider.init();
-        },
+    options: {
+      type: Object,
+      default() {
+        return {
+          init: false,
+          grabCursor: true,
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          }
+        };
+      }
     },
 
-    methods: {
-        onEndReached() {
-            this.$emit('reachEnd');
-        },
-    },
+    shouldInitialize: {
+      type: Boolean,
+      default: true
+    }
+  },
 
-    mounted() {
-        if (this.slider) {
-            this.slider.on('reachEnd', this.onEndReached);
-            if (this.shouldInitialize) this.slider.init();
-        }
-    },
+  watch: {
+    shouldInitialize(value) {
+      if (value && !this.initialized) this.slider.init();
+    }
+  },
 
-    beforeDestroy() {
-        if (this.slider) {
-            this.slider.destroy(true, false);
-            this.slider = null;
-        }
-    },
+  methods: {
+    onEndReached() {
+      this.$emit("reachEnd");
+    }
+  },
+
+  mounted() {
+    if (this.slider) {
+      this.slider.on("reachEnd", this.onEndReached);
+      if (this.shouldInitialize) this.slider.init();
+    }
+  },
+
+  beforeDestroy() {
+    if (this.slider) {
+      this.slider.destroy(true, false);
+      this.slider = null;
+    }
+  }
 };
 </script>
