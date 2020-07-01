2fd7a483a9092db456eec6e5abdba148f6850e6d
Bykov Dima <fepifmel@gmail.com>
2020-07-02 01:00:33 +0300

    template targets обработка

--- frontend/src/store/modules/template.js
+++ frontend/src/store/modules/template.js
@@ -1,15 +1,23 @@
 // import axios from "axios";
 
 export default {
-  state: {},
+  state: {
+    aboutChildrenTemplate: [],
+    businessChildrenTemplate: [],
+    educationChildrenTemplate: [],
+    volunteersChildrenTemplate: [],
+  },
   mutations: {
-      setTemplate(state, payload) {
-        console.log(payload);
-      }
+    setTemplate(payload) {
+      // const about = payload.filter((item) => {
+      //   item.link.match("https://eco-z.org/education/");
+      // });
+      console.log(payload);
+    },
   },
   actions: {
     fetchTemplate({ commit }, data) {
-        commit("setTemplate", data)
+      commit("setTemplate", data);
     },
     // async fetchLinks({}) {
     //   try {
@@ -24,6 +32,22 @@ export default {
     //     console.log(e)
     //   }
     // }
+    // async fetchLinks({}) {
+    //   try {
+    //     let data = await axios
+    //       .get("https://eco-z.org//wp-json/v2/pages", {
+    //         params: {
+    //           per_page: 100,
+    //         },
+    //       })
+    //       .then((response) => response.data)
+    //       .catch((e) => console.log(e));
+    //     console.log(data);
+    //     commit("setTemplate", data);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // },
   },
   getters: {},
 };
